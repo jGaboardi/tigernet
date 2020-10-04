571a0a871b001a716ae5b14a04a7fe785d170252
James Gaboardi <jgaboardi@gmail.com>
2020-10-04 12:29:04 -0400

    initial tigernet install

--- tigernet/__init__.py
+++ tigernet/__init__.py
@@ -1,1 +1,5 @@
+__version__ = "v0.1-prealpha"
+
 #
+
+from .tigernet import TigerNet
---
+++ tigernet/tigernet.py
@@ -0,0 +1,18 @@
+"""
+"""
+
+import geopandas
+import numpy
+from libpysal import cg
+
+
+__author__ = "James D. Gaboardi <jgaboardi@gmail.com>"
+
+
+class TigerNet:
+    """
+    """
+
+    def __init__():
+
+        pass
