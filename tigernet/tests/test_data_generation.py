e18a5a03dfe3ae39539e1e5545892a7dea3b6775
James Gaboardi <jgaboardi@gmail.com>
2020-11-19 13:10:57 -0500

    adding observation generation (empir)

--- tigernet/tests/test_data_generation.py
+++ tigernet/tests/test_data_generation.py
@@ -4,6 +4,31 @@
 import tigernet
 import unittest
 import numpy
+import geopandas
+
+
+# get the roads shapefile as a GeoDataFrame
+bbox = (-84.279, 30.480, -84.245, 30.505)
+f = "zip://test_data/Edges_Leon_FL_2010.zip!Edges_Leon_FL_2010.shp"
+gdf = geopandas.read_file(f, bbox=bbox)
+gdf = gdf.to_crs("epsg:2779")
+
+# filter out only roads
+# yes_roads = gdf["ROADFLG"] == "Y"
+# roads = gdf[yes_roads].copy()
+yes_roads = gdf["MTFCC"].str.startswith("S")
+roads = gdf[yes_roads].copy()
+
+# Tiger attributes primary and secondary
+ATTR1, ATTR2 = "MTFCC", "TLID"
+
+# segment welding and splitting stipulations --------------------------------------------
+INTRST = "S1100"  # interstates mtfcc code
+RAMP = "S1630"  # ramp mtfcc code
+SERV_DR = "S1640"  # service drive mtfcc code
+SPLIT_GRP = "FULLNAME"  # grouped by this variable
+SPLIT_BY = [RAMP, SERV_DR]  # split interstates by ramps & service
+SKIP_RESTR = True  # no weld retry if still MLS
 
 
 class TestNetworkDataGeneration(unittest.TestCase):
@@ -42,14 +67,14 @@ class TestNetworkDataGeneration(unittest.TestCase):
         self.assertEqual(observed_length, known_length)
 
 
-class TestObservationDataGenerationSynth(unittest.TestCase):
+class TestObservationDataGenerationSynthetic(unittest.TestCase):
     def setUp(self):
         lat = tigernet.generate_lattice(n_hori_lines=1, n_vert_lines=1)
-        self.net = tigernet.Network(s_data=lat)
+        self.network = tigernet.Network(s_data=lat)
 
     def test_generate_observations_totalbounds(self):
         n_obs = 5
-        obs = tigernet.generate_obs(n_obs, self.net.s_data)
+        obs = tigernet.generate_obs(n_obs, self.network.s_data)
         known_n_obs = n_obs
         observed_n_obs = obs.shape[0]
         self.assertEqual(observed_n_obs, known_n_obs)
@@ -68,7 +93,7 @@ class TestObservationDataGenerationSynth(unittest.TestCase):
 
     def test_generate_observations_inbuffer(self):
         n_obs = 5
-        obs = tigernet.generate_obs(n_obs, self.net.s_data, near_net=0.5)
+        obs = tigernet.generate_obs(n_obs, self.network.s_data, near_net=0.5)
         known_n_obs = n_obs
         observed_n_obs = obs.shape[0]
         self.assertEqual(observed_n_obs, known_n_obs)
@@ -83,6 +108,92 @@ class TestObservationDataGenerationSynth(unittest.TestCase):
             ]
         )
         observed_coords = numpy.array([(p.x, p.y) for p in obs.geometry])
+        numpy.testing.assert_array_almost_equal(observed_coords, known_coords)
+
+
+class TestObservationDataGenerationEmpirical(unittest.TestCase):
+    def setUp(self):
+
+        # set up the network instantiation parameters
+        discard_segs = None
+        kwargs = {"s_data": roads.copy(), "from_raw": True}
+        attr_kws = {"attr1": ATTR1, "attr2": ATTR2}
+        kwargs.update(attr_kws)
+        comp_kws = {"record_components": True, "largest_component": True}
+        kwargs.update(comp_kws)
+        geom_kws = {"record_geom": True, "calc_len": True}
+        kwargs.update(geom_kws)
+        mtfcc_kws = {"discard_segs": discard_segs, "skip_restr": SKIP_RESTR}
+        mtfcc_kws.update({"mtfcc_split": INTRST, "mtfcc_intrst": INTRST})
+        mtfcc_kws.update({"mtfcc_split_grp": SPLIT_GRP, "mtfcc_ramp": RAMP})
+        mtfcc_kws.update({"mtfcc_split_by": SPLIT_BY, "mtfcc_serv": SERV_DR})
+        kwargs.update(mtfcc_kws)
+
+        # create a network instance
+        self.network = tigernet.Network(**kwargs)
+
+        # simplify network
+        kws = {"record_components": True, "record_geom": True, "def_graph_elems": True}
+        self.network.simplify_network(inplace=True, **kws)
+
+    def test_generate_observations_totalbounds(self):
+
+        n_obs = 500
+        obs = tigernet.generate_obs(n_obs, self.network.s_data)
+        known_n_obs = n_obs
+        observed_n_obs = obs.shape[0]
+        self.assertEqual(observed_n_obs, known_n_obs)
+
+        known_coords = numpy.array(
+            [
+                (622974.1796832045, 166162.55760926675),
+                (623169.2985719838, 165632.6814708112),
+                (622521.5219401979, 165946.95826632337),
+                (622571.9108776418, 166711.9648736473),
+                (624474.552580049, 165130.38564160923),
+            ]
+        )
+        observed_coords = numpy.array([(p.x, p.y) for p in obs.geometry])[:5]
+        numpy.testing.assert_array_almost_equal(observed_coords, known_coords)
+
+    def test_generate_observations_inbuffer(self):
+        n_obs = 500
+        obs = tigernet.generate_obs(n_obs, self.network.s_data, near_net=30)
+        known_n_obs = n_obs
+        observed_n_obs = obs.shape[0]
+        self.assertEqual(observed_n_obs, known_n_obs)
+
+        known_coords = numpy.array(
+            [
+                (622571.9108776418, 166711.9648736473),
+                (624474.552580049, 165130.38564160923),
+                (623803.6385443554, 166644.26001242414),
+                (622876.6555154349, 165227.52219813256),
+                (623400.7775349629, 166023.94388077687),
+            ]
+        )
+        observed_coords = numpy.array([(p.x, p.y) for p in obs.geometry])[:5]
+        numpy.testing.assert_array_almost_equal(observed_coords, known_coords)
+
+    def test_generate_observations_inbuffer_restrict(self):
+        n_obs = 500
+        obs = tigernet.generate_obs(
+            n_obs, self.network.s_data, restrict=("MTFCC", ["S1100"]), near_net=30
+        )
+        known_n_obs = n_obs
+        observed_n_obs = obs.shape[0]
+        self.assertEqual(observed_n_obs, known_n_obs)
+
+        known_coords = numpy.array(
+            [
+                (622571.9108776418, 166711.9648736473),
+                (624474.552580049, 165130.38564160923),
+                (623803.6385443554, 166644.26001242414),
+                (622876.6555154349, 165227.52219813256),
+                (623400.7775349629, 166023.94388077687),
+            ]
+        )
+        observed_coords = numpy.array([(p.x, p.y) for p in obs.geometry])[:5]
         numpy.testing.assert_array_almost_equal(observed_coords, known_coords)
 
 
